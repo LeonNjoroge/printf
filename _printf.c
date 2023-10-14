f05aaad6db73609102dc6f6de0e487ddadf28a4e
Leon <leonmuraguri@gmail.com>
2023-10-14 00:07:35 -0700

    Add _printf file

---
+++ _printf.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+
+int _printf(const char * const format, ...)
+{
+
+
+
+}
--- main.h
+++ main.h
@@ -7,8 +7,11 @@
 #include <limits.h>
 #include <unistd.h>
 
-int _putchar(char c);
 
+
+
+int _putchar(char c);
+int _printf(const char *format, ...);
 
 
 
