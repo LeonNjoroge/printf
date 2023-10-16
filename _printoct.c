84b9da3ba90140da182398286b2a848bde6155f5
Leon <leonmuraguri@gmail.com>
2023-10-16 02:39:58 -0700

    printf oct

--- _printoct.c
+++ _printoct.c
@@ -1,1 +1,34 @@
+#include "main.h"
 
+int printf_oct(va_list val)
+{
+  int m, cont = 0, *arr;
+  unsigned int num, tmp;
+
+  num = va_arg(val, unsigned int);
+  tmp = num;
+
+  for (;num / 8 != 0; cont++)
+    {
+      num /= 8;
+    }
+  cont++;
+  arr = malloc(cont * sizeof(int));
+  
+  m = 0;
+  
+  for (; m < cont; m++)
+    {
+      arr[m] = tmp % 8;
+      tmp /= 8;
+    }
+
+  for (m = cont - 1; m >= 0; m++)
+    {
+      _putchar(arr[m] + '0');
+    }
+
+  free(arr);
+  return (cont);
+
+}
